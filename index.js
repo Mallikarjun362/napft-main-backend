b6da088d1b638ac9aa6a1da7c1643e9b04275291
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-02-11 20:36:12 +0530

    bug fixes

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.get("/api/nft", (req, res) => {
     console.log("nft", req.body);
     try {
         if (!("start" in req.body)) {
-            res.status(400).send(`<h1>Empty parameters ${req.body}<h1>`);
+            res.status(400).send({msg:`<h1>Empty parameters ${req.body}<h1>`,body:req.body});
             return;
         }
         if (req.body.start === req.body.end) {
